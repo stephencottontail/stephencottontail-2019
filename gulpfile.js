8f9b0bc0fa105cb6cce67ac5a6062d793c55cc3f
Stephen Dickinson <stephencottontail@me.com>
2019-04-26 09:40:22 -0600

    Add autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
-var gulp = require( "gulp" );
-var sass = require( "gulp-sass" );
+var gulp    = require( "gulp" );
+var sass    = require( "gulp-sass" );
+var postCss = require( "gulp-postcss" );
 
 gulp.task( "styles", () => {
     return gulp.src( "assets/scss/*.scss" )
                .pipe( sass().on( "error", sass.logError ) )
+               .pipe( postCss() )
                .pipe( gulp.dest( "./" ) );
 } );
 
