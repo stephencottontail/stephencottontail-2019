05142d4601771b73de961d15e93192d90c846b0b
Stephen Dickinson <stephencottontail@me.com>
2019-05-01 00:26:00 -0600

    Display featured image on single views

--- lib/post.php
+++ lib/post.php
@@ -1,5 +1,28 @@
 <?php
 
+    add_action( 'genesis_entry_header', function() {
+        if ( ! is_single() ) {
+            return;
+        }
+
+        $img = genesis_get_image( array(
+            'format'  => 'html',
+            'size'    => 'large',
+            'context' => 'archive',
+            'attr'    => array( 'class' => 'aligncenter' )
+        ) );
+
+        if ( ! empty( $img ) ) {
+            genesis_markup( array(
+                'open'    => '<div %s>',
+                'close'   => '</div>',
+                'content' => wp_make_content_images_responsive( $img ),
+                'context' => 'post-thumbnail',
+            ) );
+        }
+    } );
+
+    add_action( 'genesis_post_content', 'sc2019_do_post_image' );
     add_shortcode( 'post_edit_with_title', 'sc2019_post_edit_with_title_shortcode' );
     function sc2019_post_edit_with_title_shortcode( $atts ) {
         ob_start();
