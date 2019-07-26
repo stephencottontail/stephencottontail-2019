0530fa44e15fe1c49125eb7de8b2ab55470a1c95
Stephen Dickinson <stephencottontail@me.com>
2019-07-26 17:40:56 -0600

    Developer happiness

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,16 @@
 var gulp    = require( "gulp" );
 var sass    = require( "gulp-sass" );
-var postCss = require( "gulp-postcss" );
+var postcss = require( "gulp-postcss" );
 
 gulp.task( "styles", () => {
     return gulp.src( "assets/scss/*.scss" )
                .pipe( sass().on( "error", sass.logError ) )
-               .pipe( postCss() )
+               .pipe( postcss() )
                .pipe( gulp.dest( "./" ) );
+} );
+
+gulp.task( "watch", () => {
+    gulp.watch( "assets/scss/*.scss", gulp.series( "styles" ) );
 } );
 
 gulp.task( "default", gulp.series( "styles" ) );
